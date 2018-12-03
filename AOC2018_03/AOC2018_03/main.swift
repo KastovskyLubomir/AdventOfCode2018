9ba7523690bb2b55a2f8842a71b6555d8272b65e
Lubomir Kastovsky <lubomir.kastovsky@avast.com>
2018-12-03 15:20:19 +0100

    day 3

--- AOC2018_03/AOC2018_03/main.swift
+++ AOC2018_03/AOC2018_03/main.swift
@@ -8,4 +8,47 @@
 
 import Foundation
 
+// #1 @ 1,3: 4x4
+func idWithCoordinates(square: String) -> (id: Int, coords: [Int]) {
+    var id = 0
+    var co = [Int]()
+    let args = square.components(separatedBy: ["#", "@", ",", ":", "x", " "]).compactMap { Int($0)}
+    id = args[0]
+    for row in 0..<args[4] {
+        for col in 0..<args[3] {
+            co.insert(((row+args[2])*1000) + (col+args[1]), at:co.endIndex)
+        }
+    }
+    return (id, co)
+}
 
+
+var idSet : Set<Int> = [] // for solution 2
+var space : [Int: Set<Int>] = [:]
+for line in readLinesRemoveEmpty(str: inputString) {
+    let res = idWithCoordinates(square: line)
+    idSet.insert(res.id)
+    for x in res.coords {
+        if space[x] == nil {
+            space[x] = [res.id]
+        } else {
+            space[x]?.insert(res.id)
+        }
+    }
+}
+
+var count = 0
+for key in space.keys {
+    count += space[key]!.count > 1 ? 1 : 0
+}
+
+print("1. result: ", count)
+
+// remove overlaping
+for key in space.keys {
+    if space[key]!.count > 1 {
+        idSet = idSet.subtracting(space[key]!)
+    }
+}
+
+print("2. result: ", idSet)
